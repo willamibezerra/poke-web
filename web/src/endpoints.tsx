0cb540f7cb081f9fea195b7bb263ff98833a71cd
Willami Bezerra <willamibezerra035@gmail.com>
2025-01-24 19:09:33 -0300

    Versioning untracked files

--- web/src/endpoints.tsx
+++
@@ -1,4 +0,0 @@
-export const ENDPOINTS = {
-    USERS: 'https://pokeapi.co/api/v2/pokemon?limit=20'
-  
-  };
--- web/src/types/poke_api_interface.ts
+++
@@ -1,7 +0,0 @@
-import { Ipokemon } from './pokemon';
-
-export interface IpokeApiResponse{
-    map(arg0: (pokemon: any) => import("react").JSX.Element): import("react").ReactNode;
-    results:Ipokemon[]
-    count: number;
-}
